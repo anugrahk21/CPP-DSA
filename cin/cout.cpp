9fb5683b52eab5b8a39e50650e8ed98ff9520101
ANUGRAH K <anugrah.k910@gmail.com>
2024-08-13 10:22:31 +0530

    few additions

--- cin/cout.cpp
+++ cin/cout.cpp
@@ -6,5 +6,11 @@ int main(){
     string a;
     cout<<"Enter a name: "<<endl;
     cin>>a;
-    cout<<"The entered name is: "<<a;
+    cout<<"The entered name is: "<<a<<endl;
+    int b;
+    cout<<"Enter age: "<<endl;
+    cin>>b;
+    cout<<"The age is: "<<b;
+    return 0;
+
 }
