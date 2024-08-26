246b2acffddab26d71a2254d9c0a920f38b6b772
ANUGRAH K <anugrah.k910@gmail.com>
2024-08-27 00:14:58 +0530

    occurrence of a digit

---
+++ practicep13.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+    int n;
+    cin>>n;
+    int k=n;
+    int arr[10]={0,0,0,0,0,0,0,0,0,0};
+    while(n!=0){
+        int j=n%10;
+        arr[j]+=1;;
+        n=n/10;
+    };
+    
+    for(int i=0;i<10;++i){
+        
+        if (arr[i]>=1){
+            cout<<"Digit "<<i<<":  "<<arr[i]<<" times"<<endl;
+        }
+    };
+    
+};
