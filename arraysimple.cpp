18b31cea61a5e9a7c072d0808ecbfea392058345
ANUGRAH K <anugrah.k910@gmail.com>
2024-09-09 23:08:26 +0530

    added multiplication table

--- arraysimple.cpp
+++ arraysimple.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
 using namespace std;
+
+int table(int number){
+    int mul[10];
+    for(int i=1; i<11; ++i){
+        mul[i]=number*i;
+    }
+    for(int j=0; j<10; ++j){
+        cout << mul[j] << " ";
+    }
+    return 0; 
+}
 int main(){
     int arr[5];
     for(int j=0; j<5; ++j){
@@ -10,4 +21,9 @@ int main(){
     for(int l=0; l<5; ++l){
         cout << arr[l]<<" ";
     }
+    cout << endl;
+    cout<<"Table of which number:" ;
+    int num;
+    cin >> num;
+    table(num);
 }
