27269eda0db3f0de413c34c1dbbc51f302f2e47c
ANUGRAH K <anugrah.k910@gmail.com>
2024-09-13 13:58:59 +0530

    pointers, dangling pointers

--- practicep22.cpp
+++ practicep22.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 using namespace std;
 
+//pointers
+
 int main(){
     int a=4;
     int *p=&a; //points to the value stored in the address of a
@@ -16,6 +18,26 @@ int main(){
     *p=6;
     cout<<a<<endl;
     cout<<*p<<endl; //prints 10.
+
+    //Dangling Pointers
+    int *ptr;
+    {
+        int v=23;
+        ptr=&v;
+        cout<<"Value is(inside block): "<<*ptr<<endl;
+        cout<<"Address is(inside block): "<<ptr<<endl;
+    }
+    //still we are able to access
+    cout<<"Value is(outside block): "<<*ptr<<endl;
+    cout<<"Address is(outside block): "<<ptr<<endl;
+    //to overcome this
+    //we make ptr as NULL
+    ptr=NULL;
+    //*ptr=NULL will only make the value inside ptr as NULL not the pointer itself.
+
+    //the below code wont print as ptr is NULL(no pointer)
+    cout<<"value after null(outside block): "<<*ptr<<endl;
+    cout<<"Address after null(outside block): "<<ptr<<endl;
     return 0;
 
 }
